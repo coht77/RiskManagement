b2ea8280160cd4eaf529bd46b37921a0d81b341a
Carlos Hidalgo <carlos.hidalgo@gmail.com>
2026-08-14 17:40:05 +0000

    Test

--- srv/service.js
+++ srv/service.js
@@ -7,6 +7,7 @@ module.exports = cds.service.impl(async function () {
     // Delegar ÚNICAMENTE las lecturas de la entidad remota a S/4HANA / API Hub
     // CAP se encarga automáticamente de optimizar las consultas del Value Help y los $expand de Fiori
     this.on("READ", "A_BusinessPartner", async (req) => {
-        return BPsrv.run(req.query);
+        return BPsrv.run(req.query); 
+        
     });
 });
